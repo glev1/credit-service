9dcfcfe8dee2040c364e72e96e7267bde5ed54e3
glev1 <guilevi880@gmail.com>
2022-01-18 20:48:32 -0300

    Add load_train_data function in data_utils

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import numpy as np
 import logging
+
+logger = logging.getLogger(__name__)
+
+def load_train_data() -> pd.DataFrame:
+    """Load Credit Service data from csv file"""
+    logger.info('Loading data')
+    file = '../../datasets/cs-training.csv'
+    df_train = pd.read_csv(file)
+    return df_train
