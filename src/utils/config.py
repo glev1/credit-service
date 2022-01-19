ef10f92eb3af8fe0510127b64e662b30cecf4a51
glev1 <guilevi880@gmail.com>
2022-01-18 21:50:21 -0300

    Add get_default function in config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -8,3 +8,23 @@ def set_main_path() -> str:
         os.path.dirname(
             os.path.dirname(
                 os.path.abspath(__file__))))
+
+def get_default(config_name: str, parameter: str):
+    """Get default configuration values.
+
+    Args:
+        config_name (str): Name of the config file.
+        parameter (str): Name of the config parameter being loaded.
+
+    Returns:
+        The value corresponding to the requested key.
+
+    """
+    main_path = set_main_path()
+    file = os.path.join(main_path, f"src/conf/{config_name}.json")
+
+    with open(file, encoding='utf-8') as f:
+        data = json.load(f)
+    return data[parameter]
+
+        
