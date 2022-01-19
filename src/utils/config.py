dcdbcbc9a40cb1fdb7cf57c58b45a31a9f3a80b7
glev1 <guilevi880@gmail.com>
2022-01-18 21:45:45 -0300

    Add config.py

---
+++ src/utils/config.py
@@ -0,0 +1,10 @@
+import os
+import json
+
+def set_main_path() -> str:
+    """Find main path to the project"""
+
+    return os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(
+                os.path.abspath(__file__))))
