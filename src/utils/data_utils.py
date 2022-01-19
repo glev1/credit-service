0f671795bcf60711ea46af5013abe1851d3ffc26
glev1 <guilevi880@gmail.com>
2022-01-18 21:36:13 -0300

    Change variable data_utils

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -2,11 +2,14 @@ import pandas as pd
 import numpy as np
 import logging
 
+from sklearn.model_selection import train_test_split
+from typing import Tuple
+
 logger = logging.getLogger(__name__)
 
-def load_train_data() -> pd.DataFrame:
+def load_data() -> pd.DataFrame:
     """Load Credit Service data from csv file"""
     logger.info('Loading data')
     file = '../../datasets/cs-training.csv'
-    df_train = pd.read_csv(file)
-    return df_train
+    df = pd.read_csv(file)
+    return df
