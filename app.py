68cf7bb6089febbd84a528d15bbbb4a5410cdbc0
glev1 <guilevi880@gmail.com>
2022-01-27 19:14:19 -0300

    Add mk_web_request

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import ast
 import logging
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
@@ -11,7 +12,11 @@ LOG.setLevel(logging.INFO)
 @app.route('/')
 def home():
     """Return a HTTP greeting"""
-    html = f"<h3>Credit Service</h3>"
+    html = '''<h1 style="color:green;" >Welcome to the Give Me Some Credit Web Application</h1>
+            <p>Banks play a crucial role in market economies. They decide who can get finance and on what terms and can make <br>
+            or break investment decisions. For markets and society to function, individuals and companies need access to credit. <br>
+            <p>Here, you can predict the probability that somebody will experience financial disstress in the next two years.</p>
+            <p>For more information, access <a href="https://github.com/glev1/credit-service">Credit Service Repo</a>.'''
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
@@ -22,6 +27,16 @@ def mk_predict():
     LOG.info(f"JSON payload: {json_payload}")
 
     prediction = predict(json_payload)["human_readable_predict"]
+
+    return jsonify(prediction)
+
+@app.route("/webpredict/<profile>")
+def mk_web_predict(profile):
+    """Predicts the chance of defaulting"""
+    payload = ast.literal_eval(profile)
+
+    LOG.info(f"JSON payload: {payload}")
+    prediction = predict(payload)["human_readable_predict"]
 
     return jsonify(prediction)
 
